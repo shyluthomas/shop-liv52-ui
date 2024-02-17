4b28bb97f88f7ea9afef4eefdcb2487292221f61
shyluthomas <shylupthomas@gmail.com>
2024-02-17 17:48:28 +0000

    Step: Update env varibales
    Signed-off-by: shyluthomas <shylupthomas@gmail.com>

---
+++ src/services/commonService.ts
@@ -0,0 +1,13 @@
+import { axiosNonAuth } from "@/utils/axios";
+
+const API_URL = import.meta.env.VITE_API;
+
+export const commonService = {
+
+    login: async () => {
+        // body here
+        // use axiosNonAuth for post and get
+
+    }
+
+}
